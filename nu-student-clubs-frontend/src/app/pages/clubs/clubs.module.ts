5e930770a08a41d7594a7333be79146835d79aa3
Gehad Almolla <gehad.almolla@gmail.com>
2025-12-19 02:22:40 +0200

    fix: update clubs module to import standalone components

--- nu-student-clubs-frontend/src/app/pages/clubs/clubs.module.ts
+++ nu-student-clubs-frontend/src/app/pages/clubs/clubs.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ClubListComponent } from './club-list/club-list';
 import { ClubDetailsComponent } from './club-details/club-details';
@@ -22,7 +21,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    CommonModule,
+    ClubListComponent,
+    ClubDetailsComponent,
+    MyMembershipsComponent,
     RouterModule.forChild(routes)
   ]
 })
